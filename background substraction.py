893f389e0d182a2693e78acdf8db31c232fa0542
Madhav Agarwal <madhavagarwal99@gmail.com>
2017-07-18 03:36:28 +0530

    Add files via upload

---
+++ background substraction.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 00:08:58 2017
+
+@author: Madhav
+"""
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('D:/DataScience/OpenCV/cctv.mp4')
+
+fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
+
+while(1):
+    ret, frame = cap.read()
+    
+    fgmask = fgbg.apply(frame)
+
+    cv2.imshow('frame',fgmask)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
